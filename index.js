cbf81487efadda03f196387d75be17f6757d80e6
JakSOt599 <98386561+JakSOt599@users.noreply.github.com>
2022-01-25 11:32:01 +0100

    Uploaded files

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const client = new Discord.Client(
+    { intents: [ 'GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES' ] }
+)
+
+client.login('OTM1NDA3MjQ1MDgzMTgxMDY2.Ye-L4g.YrfpMWfuJjOc-RGeL1DSaRlJYxA')
+
+client.on('ready', () => {
+    console.log('Bot ONLINE')
+} )
+
+client.on('messageCreate', (message) => {
+    if (message.content == '!youtube') {
+        message.channel.send('Questo è il mio canale: https://www.youtube.com/')
+    }
+
+    if(message.content == 'Ciao') {
+        message.channel.send('Benvenuto nella nostra community')
+    }
+
+    if (message.content == '!embed') {
+        var embed = new Discord.MessageEmbed()
+            .setTitle('Titolo embed')
+            .setDescription(`${message.author.username} ha scritto il messaggio`)
+        message.channel.send({ embeds: [embed] })
+    }
+} )
